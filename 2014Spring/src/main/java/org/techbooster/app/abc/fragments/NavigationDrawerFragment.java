a6382f2ae5bba263638fa1cc127501e62b26f909
Toshihiro Yagi <toshihiro-yagi@cookpad.com>
2014-02-18 18:19:23 +0900

    Add menu list.

--- 2014Spring/src/main/java/org/techbooster/app/abc/fragments/NavigationDrawerFragment.java
+++ 2014Spring/src/main/java/org/techbooster/app/abc/fragments/NavigationDrawerFragment.java
@@ -110,11 +110,7 @@ public class NavigationDrawerFragment extends Fragment {
                 getActionBar().getThemedContext(),
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
-                new String[]{
-                        getString(R.string.title_section1),
-                        getString(R.string.title_section2),
-                        getString(R.string.title_section3),
-                }));
+                getResources().getStringArray(R.array.drawer_menu_list)));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
         return mDrawerListView;
     }
